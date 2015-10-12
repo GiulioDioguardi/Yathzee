1877237cd2a9a013d1f046eaf79cf92f70f74d4a
Giulio Dioguardi <giulio.dioguardi@alten.nl>
2015-10-12 11:24:20 +0200

    Added more unit tests for UpperScoreSection

--- src/YathzeeUnitTest/UnitTest1.cs
+++ src/YathzeeUnitTest/UnitTest1.cs
@@ -43,16 +43,67 @@ namespace YathzeeUnitTest
     public class UpperScoreSectionTest
     {
         [TestMethod]
-        public void OneTest()
+        public void OneThreeFourTest()
         {
             UpperScoreSection upperSection = new UpperScoreSection();
             Die[] dice = new Die[5] { new Die(3), new Die(1), new Die(1), new Die(4), new Die(6) };
-            Dictionary<string, int> one = new Dictionary<string, int>();
-            one = upperSection.checkScore(dice, 1);
-            Assert.AreEqual(2, one["one"], "Error in ones");
-            Dictionary<string, int> three = new Dictionary<string, int>();
-            three = upperSection.checkScore(dice, 3);
-            Assert.AreEqual(3, three["three"], "Error in threes");
+            Dictionary<string, int> upper = new Dictionary<string, int>();
+            upper = upperSection.checkScore(dice, 1);
+            Assert.AreEqual(2, upper["one"], "Error in ones");
+            upper = upperSection.checkScore(dice, 3);
+            Assert.AreEqual(3, upper["three"], "Error in threes");
+            upper = upperSection.checkScore(dice, 4);
+            Assert.AreEqual(4, upper["four"], "Error in fours");
+        }
+
+        [TestMethod]
+        public void ZeroScoreTest()
+        {
+            UpperScoreSection upperSection = new UpperScoreSection();
+            Die[] dice = new Die[5] { new Die(5), new Die(2), new Die(3), new Die(6), new Die(6) };
+            Dictionary<string, int> upper = new Dictionary<string, int>();
+            upper = upperSection.checkScore(dice, 1);
+            Assert.AreEqual(0, upper["one"], "Error in ones, zero score expected");
+        }
+
+        [TestMethod]
+        public void TotalTest()
+        {
+
+            UpperScoreSection upperSection = new UpperScoreSection();
+            Dictionary<string, int> scoreValues = new Dictionary<string, int>();
+            Die[] dice = new Die[5] { new Die(6), new Die(6), new Die(6), new Die(2), new Die(4) };
+            scoreValues = upperSection.checkScore(dice, 6);
+            Assert.AreEqual(18, scoreValues["total"]);
+            dice = new Die[5] { new Die(6), new Die(6), new Die(2), new Die(2), new Die(4) };
+            scoreValues = upperSection.checkScore(dice, 2);
+            Assert.AreEqual(22, scoreValues["total"]);
+        }
+        [TestMethod]
+        public void BonusTest()
+        {
+            UpperScoreSection upperSection = new UpperScoreSection();
+            Dictionary<string, int> upperBonusEarned = new Dictionary<string, int>();
+
+            Die[] dice = new Die[5] { new Die(1), new Die(1), new Die(1), new Die(2), new Die(4) };
+            upperBonusEarned = upperSection.checkScore(dice, 1);
+
+            dice = new Die[5] { new Die(2), new Die(6), new Die(2), new Die(2), new Die(4) };
+            upperBonusEarned = upperSection.checkScore(dice, 2);
+
+            dice = new Die[5] { new Die(3), new Die(3), new Die(6), new Die(3), new Die(4) };
+            upperBonusEarned = upperSection.checkScore(dice, 3);
+
+            dice = new Die[5] { new Die(4), new Die(5), new Die(4), new Die(2), new Die(4) };
+            upperBonusEarned = upperSection.checkScore(dice, 4);
+
+            dice = new Die[5] { new Die(6), new Die(5), new Die(6), new Die(5), new Die(5) };
+            upperBonusEarned = upperSection.checkScore(dice, 5);
+
+            dice = new Die[5] { new Die(6), new Die(6), new Die(6), new Die(2), new Die(4) };
+            upperBonusEarned = upperSection.checkScore(dice, 6);
+
+            Assert.AreEqual(35, upperBonusEarned["bonus"], "Bonus is not given when it should");
         }
     }
 }
