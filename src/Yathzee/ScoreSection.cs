584f0c1dfca1b76924b8afa18cfb5cccc0980c46
Giulio Dioguardi <giulio.dioguardi@alten.nl>
2015-10-12 10:53:07 +0200

    Added class for containing scores in score section; added unit tests for scoresection

---
+++ src/Yathzee/ScoreSection.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yathzee
+{
+    public abstract class ScoreSection
+    {
+        private int total;
+        public ScoreSection()
+        { }
+
+        public int getTotal()
+        {
+            return total;
+        }
+
+        public void setTotal(int value)
+        {
+            total = value;
+        }
+
+        private bool areAllSet()
+        {
+            return false;
+        }
+    }
+
+    public class UpperScoreSection : ScoreSection
+    {
+        private Dictionary<string, int> scoreValues = new Dictionary<string, int>(
+            );
+        private bool oneIsSet = false, twoIsSet = false, threeIsSet = false,
+            fourIsSet = false, fiveIsSet = false, sixIsSet = false, isInitialized = false;
+
+        private void initializeScoreValues()
+        {
+            scoreValues.Add("one", 0);
+            scoreValues.Add("two", 0);
+            scoreValues.Add("three", 0);
+            scoreValues.Add("four", 0);
+            scoreValues.Add("five", 0);
+            scoreValues.Add("six", 0);
+            scoreValues.Add("bonus", 0);
+            scoreValues.Add("total", getTotal());
+        }
+
+        public Dictionary<string, int> checkScore(Die[] dice, int checkedValue)
+        {
+            if (!isInitialized)
+            {
+                initializeScoreValues();
+                isInitialized = true;
+            }
+            switch (checkedValue)
+            {
+                case 1:
+                    scoreValues["one"] = sumScore(dice, checkedValue);
+                    oneIsSet = true;
+                    break;
+                case 2:
+                    scoreValues["two"] = sumScore(dice, checkedValue);
+                    twoIsSet = true;
+                    break;
+                case 3:
+                    scoreValues["three"] = sumScore(dice, checkedValue);
+                    threeIsSet = true;
+                    break;
+                case 4:
+                    scoreValues["four"] = sumScore(dice, checkedValue);
+                    fourIsSet = true;
+                    break;
+                case 5:
+                    scoreValues["five"] = sumScore(dice, checkedValue);
+                    fiveIsSet = true;
+                    break;
+                case 6:
+                    scoreValues["six"] = sumScore(dice, checkedValue);
+                    sixIsSet = true;
+                    break;
+                default:
+                    break;
+            }
+
+            if (areAllSet())
+            {
+                if(isBonusEarned())
+                {
+                    scoreValues["bonus"] = 35;
+                }
+            }
+            setTotal(sumTotal());
+            scoreValues["total"] = getTotal();
+            return scoreValues;
+        }
+
+        private bool isBonusEarned()
+        {
+            int sum = scoreValues["one"] + scoreValues["two"] + scoreValues["three"] + 
+                scoreValues["four"] + scoreValues["five"] + scoreValues["six"];
+            if (sum <= 63)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool areAllSet()
+        {
+            if (oneIsSet && twoIsSet && threeIsSet && fourIsSet && fiveIsSet && sixIsSet)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private int sumTotal()
+        {
+            return scoreValues["one"] + scoreValues["two"] + scoreValues["three"] + 
+                scoreValues["four"] + scoreValues["five"] + scoreValues["six"] + 
+                scoreValues["bonus"];
+        }
+
+        private int sumScore(Die[] dice, int dieValue)
+        {
+            int sum = 0;
+            for (int i = 0; i < dice.Length; i++)
+            {
+                if (dice[i].getDieValue() == dieValue)
+                {
+                    sum += dieValue;
+                }
+            }
+            return sum;
+        }
+    }
+
+    public class LowerScoreSection : ScoreSection
+    {
+
+    }
+
+}
--- src/YathzeeUnitTest/UnitTest1.cs
+++ src/YathzeeUnitTest/UnitTest1.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 using Yathzee;
 
 namespace YathzeeUnitTest
 {
     [TestClass]
-    public class UnitTest1
+    public class DieTests
     {
         [TestMethod]
         public void DieTest()
@@ -17,13 +18,13 @@ namespace YathzeeUnitTest
             }
         }
 
-      /*  [TestMethod]
-        public void DieImageTest()
-        {
-            Die die = new Die(0);
-            DieImage image = new DieImage();
-            Assert.AreEqual(image.getDieBlank(), die.getDieImage());
-        }*/
+        /*  [TestMethod]
+          public void DieImageTest()
+          {
+              Die die = new Die(0);
+              DieImage image = new DieImage();
+              Assert.AreEqual(image.getDieBlank(), die.getDieImage());
+          }*/
 
         [TestMethod]
         public void DiceGeneratorTest()
@@ -35,6 +36,23 @@ namespace YathzeeUnitTest
                 Assert.IsTrue(die.getDieValue() <= 6, "Die shows a value too high.");
                 Assert.IsTrue(die.getDieValue() > 0, "Die shows a value too low.");
             }
+        }
+    }
+
+    [TestClass]
+    public class UpperScoreSectionTest
+    {
+        [TestMethod]
+        public void OneTest()
+        {
+            UpperScoreSection upperSection = new UpperScoreSection();
+            Die[] dice = new Die[5] { new Die(3), new Die(1), new Die(1), new Die(4), new Die(6) };
+            Dictionary<string, int> one = new Dictionary<string, int>();
+            one = upperSection.checkScore(dice, 1);
+            Assert.AreEqual(2, one["one"], "Error in ones");
+            Dictionary<string, int> three = new Dictionary<string, int>();
+            three = upperSection.checkScore(dice, 3);
+            Assert.AreEqual(3, three["three"], "Error in threes");
         }
     }
 }
