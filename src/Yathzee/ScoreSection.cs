9147c124be8118a98b9148aef9ee97be58864866
Giulio Dioguardi <giulio.dioguardi@alten.nl>
2016-04-19 21:44:46 +0200

    Updated documentation for Abstract Score Section

--- src/Yathzee/ScoreSection.cs
+++ src/Yathzee/ScoreSection.cs
@@ -6,8 +6,14 @@ using System.Threading.Tasks;
 
 namespace Yathzee
 {
+    /// <summary>
+    /// Abstract class for score section.
+    /// </summary>
     public abstract class ScoreSection
     {
+        /// <summary>
+        /// Attribute that holds the total value of a score section.
+        /// </summary>
         private int total;
         public int Total { get { return total; } set { total = value; } }
     }
