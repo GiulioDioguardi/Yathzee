21280942e1d026971ae75987b7951bed47ef2bcd
Giulio Dioguardi <giulio.dioguardi@alten.nl>
2016-04-19 20:53:05 +0200

    Created inline documentation for Dice generator

--- src/Yathzee/DiceGenerator.cs
+++ src/Yathzee/DiceGenerator.cs
@@ -9,11 +9,18 @@ namespace Yathzee
 {
     public class DiceGenerator
     {
+        /// <summary>
+        /// Class responsible for creating Die objects.
+        /// </summary>
         Random rnd;
         public DiceGenerator()
         {
             rnd = new Random();
         }
+        /// <summary>
+        /// Creates a Die object using a random number generator.
+        /// </summary>
+        /// <returns>A die object.</returns>
         public Die generateDice()
         {    
             int value = rnd.Next(6) + 1;
